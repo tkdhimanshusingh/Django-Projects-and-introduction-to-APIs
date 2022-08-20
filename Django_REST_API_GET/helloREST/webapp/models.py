88b8d0c555205039affa2e43e2291c460bd46bc1
Himanshu Singh <tkdhimanshusingh@gmail.com>
2022-08-20 09:31:28 +0530

    Push intro_site, Forms site, GET API.

---
+++ Django_REST_API_GET/helloREST/webapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.forms import IntegerField
+from sympy import Integer
+
+# Create your models here.
+class employees(models.Model):
+    firstname=models.CharField(max_length=10)
+    lastname=models.CharField(max_length=10)
+    emp_id=models.IntegerField()
+
+    def __str__(self):
+        return self.firstname
---
+++ Django_REST_API_GET/helloREST/webapp/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from . models import employees
+class employeesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=employees
+        fields='__all__'
